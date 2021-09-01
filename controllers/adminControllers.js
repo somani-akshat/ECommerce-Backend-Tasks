8ac251ae02ea90c965e4588cf3213e5f8db50357
Raj <rajsanghavi9@gmail.com>
2021-09-01 18:40:04 +0530

    Added route for deleting a user by admin

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -12,4 +12,19 @@ const getAllUserDetails = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getAllUserDetails };
+// Delete a user account
+const deleteUser = asyncHandler(async (req, res) => {
+  const userExists = await User.findById(req.params.id);
+  if (userExists) {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "User Account deleted successfully",
+    });
+  } else {
+    res.status(400).json({
+      message: "No user found",
+    });
+  }
+});
+
+module.exports = { getAllUserDetails, deleteUser };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
-const { getAllUserDetails } = require("../controllers/adminControllers");
+const {
+  getAllUserDetails,
+  deleteUser,
+} = require("../controllers/adminControllers");
 
 const { adminProtect } = require("../middlewares/protectedRoutes");
 
 const router = express.Router();
 
-// Register new user
+// Get all user details
 router.route("/allUserDetails").get(adminProtect, getAllUserDetails);
+// Delete user account
+router.route("/deleteUser/:id").delete(adminProtect, deleteUser);
 
 module.exports = router;
